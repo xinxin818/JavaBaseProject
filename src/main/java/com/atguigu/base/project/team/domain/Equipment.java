022592bade65d08a98cc8f55a9b2599b54f61540
xinxin818 <944478813@>
2020-08-03 22:49:15 +0800

    test project team commit

---
+++ src/main/java/com/atguigu/base/project/team/domain/Equipment.java
@@ -0,0 +1,6 @@
+package com.atguigu.base.project.team.domain;
+
+public interface Equipment {
+	
+	String getDescription();
+}
---
+++ src/main/java/com/atguigu/base/project/team/junit/NameListServiceTest.java
@@ -0,0 +1,35 @@
+package com.atguigu.base.project.team.junit;
+
+import org.junit.Test;
+
+import com.atguigu.base.project.team.domain.Employee;
+import com.atguigu.base.project.team.service.NameListService;
+import com.atguigu.base.project.team.service.TeamException;
+
+public class NameListServiceTest {
+	
+	
+	@Test
+	public void testGetAllEmployees(){
+		NameListService listService = new NameListService();
+		
+		Employee[] employees = listService.getAllEmployees();
+		
+		for(int i = 0;i < employees.length;i++){
+			System.out.println(employees[i]);
+		}
+	}
+	
+	@Test
+	public void testGetEmployee(){
+		int id = 2;
+		NameListService listService = new NameListService();
+		try {
+			Employee emp = listService.getEmployee(id);
+			System.out.println(emp);
+		} catch (TeamException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+}
